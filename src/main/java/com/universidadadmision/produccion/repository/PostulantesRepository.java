881547e1749efbbe13905a587329da38e969fd97
Wilson Barrantes <wabb84@gmail.com>
2024-07-24 16:15:24 -0500

    Lista Postulantes

--- src/main/java/com/universidadadmision/produccion/repository/PostulantesRepository.java
+++ src/main/java/com/universidadadmision/produccion/repository/PostulantesRepository.java
@@ -25,14 +25,17 @@ import jakarta.persistence.PersistenceContext;
 public interface PostulantesRepository extends JpaRepository<Postulantes, Long>  {
 	
 	@Transactional(readOnly=true)
-	@Query(value = "select a.id, a.persona_id as personaid,c.abreviatura as tipodocumento, b.apellido_paterno +' '+ b.apellido_materno +' '+ b.nombre as nombrepostulante,b.nro_documento,\r\n"
+	@Query(value = "select a.id, a.persona_id as personaid,c.abreviatura as tipodocumento, b.apellido_paterno, b.apellido_materno, b.nombre,b.nro_documento,\r\n"
 			+ "   a.vacante_id,a.codigo,a.grupo_id,d.nombre as nombregrupo,a.modalidad_ingreso_id,e.nombre as nombremodalidad,a.estado_postulante,a.estado_auditoria as estado,\r\n"
-			+ "   b.sexo,b.direccion,b.email,b.fecha_nacimiento,b.celular,b.telefono,b.ubigeo_id\r\n"
+			+ "   b.sexo,b.direccion,b.email,b.fecha_nacimiento,b.celular,b.telefono,b.ubigeo_id, f.nombre as carrera, g.nombre as sede\r\n"
 			+ "   from Admision.postulantes a\r\n"
 			+ "   inner join general.persona b on b.id = a.persona_id\r\n"
 			+ "	  inner join general.Catalogo c on c.id = b.cat_tipo_documento_id\r\n"
 			+ "	  inner join Admision.grupo d on d.id = a.grupo_id\r\n"
 			+ "	  inner join Admision.modalidad e on e.id = a.modalidad_ingreso_id\r\n"
+			+ "	  inner join admision.vacantes h on h.id = a.vacante_id\r\n"
+			+ "   inner join General.Carrera f on f.id = h.carrera_id\r\n"
+			+ "   inner join General.Sede g on g.id = h.sede_id\r\n"
 			+ "   order by a.id", nativeQuery = true )
 	public List<PostulantesDto> ListaGrupo();
 	
